b715644b401e44fcc7b58b59fb075fed47a85e6e
Lyriceron <quocviet2101998@gmail.com>
2018-02-28 09:35:22 +0700

    Add files via upload

---
+++ so_sanh_ba_so_nguyen.rb
@@ -0,0 +1,17 @@
+a = (print "enter a value for a: "; gets).to_i
+b = (print "enter a value for b: "; gets).to_i
+c = (print "enter a value for c: "; gets).to_i
+
+max = a
+if max < b
+   max = b
+end
+if max < c 
+   max = c
+end
+puts "max = #{max}" 
+		                 	
+	
+		
+
+		
